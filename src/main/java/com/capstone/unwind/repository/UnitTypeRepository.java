14419fddb7840c65fc10b82b2e8c3df86f8a4481
Thanh Long Đặng <dangthanhlong211@gmail.com>
2024-10-22 10:07:22 +0700

    featt/deactive

--- src/main/java/com/capstone/unwind/repository/UnitTypeRepository.java
+++ src/main/java/com/capstone/unwind/repository/UnitTypeRepository.java
@@ -1,12 +1,17 @@
 package com.capstone.unwind.repository;
 
 import com.capstone.unwind.entity.UnitType;
+import com.capstone.unwind.entity.UnitTypeAmenity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UnitTypeRepository extends JpaRepository<UnitType, Integer> {
     List<UnitType> findAllByResortId(Integer resortId);
+    List<UnitType> findAllByResortIdAndIsActiveTrue(Integer resortId);
 
+    Optional<UnitType> findByIdAndIsActiveTrue(Integer id);
 }
