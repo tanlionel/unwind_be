ef6510507ee9ddaa5e6bd0995e989d75bfd79b08
Nguyễn Tấn <76957209+tanlionel@users.noreply.github.com>
2024-12-08 11:55:17 +0700

    fcm flow

---
+++ src/main/java/com/capstone/unwind/entity/Notification.java
@@ -0,0 +1,44 @@
+package com.capstone.unwind.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "notification")
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title", length = 45)
+    private String title;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "is_read", nullable = false)
+    private Boolean isRead ;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "type", length = 20)
+    private String type;
+
+    @Column(name = "role", length = 20)
+    private String role;
+
+}
---
+++ src/main/java/com/capstone/unwind/service/ServiceInterface/NotificationService.java
@@ -0,0 +1,20 @@
+package com.capstone.unwind.service.ServiceInterface;
+
+
+import com.capstone.unwind.entity.Notification;
+import com.capstone.unwind.exception.OptionalNotFoundException;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface NotificationService {
+    Page<Notification> getAllNotificationCustomer(Integer pageNo, Integer pageSize);
+
+    Page<Notification> getAllNotificationByTopic(String topic,Integer pageNo, Integer pageSize);
+
+    Notification markReadById(Integer notiId) throws OptionalNotFoundException;
+
+    Boolean markAllReadByUserId(Integer userId);
+
+    Boolean markAllReadByTopic(String topic);
+}
