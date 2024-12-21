fc4a8e0be0b763f62c4a7dcb3719e011a985aaac
Nguyễn Tấn <76957209+tanlionel@users.noreply.github.com>
2024-12-21 10:34:22 +0700

    refactor code

--- src/main/java/com/capstone/unwind/model/BookingDTO/ExchangeBookingDetailMapper.java
+++ src/main/java/com/capstone/unwind/model/BookingDTO/ExchangeBookingDetailMapper.java
@@ -7,17 +7,17 @@ import org.mapstruct.*;
 public interface ExchangeBookingDetailMapper {
 
     ExchangeBooking toEntity(ExchangeBookingDetailDto exchangeBookingDetailDto);
-    @Mapping(source = "exchangePosting.roomInfo.unitType.resort.id",target = "roomInfo.unitType.resortId")
-    @Mapping(source = "exchangePosting.roomInfo.unitType.resort.resortName",target = "roomInfo.unitType.resortName")
-    @Mapping(source = "exchangePosting.roomInfo.unitType.resort.logo",target = "roomInfo.resortLogo")
-    @Mapping(source = "exchangePosting.roomInfo.unitType.resort.description",target = "roomInfo.unitType.resortDescription")
-    @Mapping(source = "exchangePosting.roomInfo.unitType.resort.location",target = "roomInfo.unitType.location")
-    @Mapping(source = "exchangePosting.roomInfo.unitType.title",target = "roomInfo.unitType.title")
-    @Mapping(source = "exchangePosting.roomInfo.unitType.price",target = "roomInfo.unitType.price")
-    @Mapping(source = "exchangePosting.roomInfo.unitType.description",target = "roomInfo.unitType.description")
-    @Mapping(source = "exchangePosting.roomInfo.unitType.photos",target = "roomInfo.unitType.photos")
-    @Mapping(source = "exchangePosting.roomInfo.roomInfoCode",target = "roomInfo.roomInfoCode")
-    @Mapping(source = "exchangePosting.roomInfo.roomInfoName",target = "roomInfo.roomInfoName")
+    @Mapping(source = "roomInfo.unitType.resort.id", target = "roomInfo.unitType.resortId")
+    @Mapping(source = "roomInfo.unitType.resort.resortName", target = "roomInfo.unitType.resortName")
+    @Mapping(source = "roomInfo.unitType.resort.logo", target = "roomInfo.resortLogo")
+    @Mapping(source = "roomInfo.unitType.resort.description", target = "roomInfo.unitType.resortDescription")
+    @Mapping(source = "roomInfo.unitType.resort.location", target = "roomInfo.unitType.location")
+    @Mapping(source = "roomInfo.unitType.title", target = "roomInfo.unitType.title")
+    @Mapping(source = "roomInfo.unitType.price", target = "roomInfo.unitType.price")
+    @Mapping(source = "roomInfo.unitType.description", target = "roomInfo.unitType.description")
+    @Mapping(source = "roomInfo.unitType.photos", target = "roomInfo.unitType.photos")
+    @Mapping(source = "roomInfo.roomInfoCode", target = "roomInfo.roomInfoCode")
+    @Mapping(source = "roomInfo.roomInfoName", target = "roomInfo.roomInfoName")
     ExchangeBookingDetailDto toDto(ExchangeBooking exchangeBooking);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
